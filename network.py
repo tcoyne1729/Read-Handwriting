05b36c688e8f8cd92d3a00a3bf1c088833b43cc6
tcoyne1729 <tcoyne1729@gmail.com>
2016-03-03 17:17:03 +0000

    Create network.py

---
+++ network.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Network(object):
+    """Create a neural network with a list [a,b,c,...,d] which gives a inputs, d outputs and all others are the number of neurons at each other level"""
+    
+    def __init__(self,nodes):
+        self.layers = len(nodes)
+        self.neurons = nodes
